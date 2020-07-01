cef7878384a0b8fb59dde230f65e36bdac304ef5
anant <34976779+anantjakhmola@users.noreply.github.com>
2020-07-01 23:25:41 +0530

    Update data.py

--- data.py
+++ data.py
@@ -1,8 +1,8 @@
 import csv
-with open('logscsv.csv',newline='') as f:
+with open('/home/anant/logger/logscsv.csv',newline='') as f:
     r = csv.reader(f)
     data = [line for line in r]
-with open('logscsv.csv','w',newline='') as f:
+with open('/home/anant/logger/logscsv.csv','w',newline='') as f:
     w = csv.writer(f)
     w.writerow(['IP','Date','Time','URL','GET/POST','PORT'])
     w.writerows(data)
